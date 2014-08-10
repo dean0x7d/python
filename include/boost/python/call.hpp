12c8c98bfdb4732e9356c8ee9ae84f6b3424c2ee
Dean Moldovan <dean0x7d@gmail.com>
2014-08-11 00:41:06 +0200

    Implemented call and call_method using variadic templates

--- include/boost/python/call.hpp
+++ include/boost/python/call.hpp
@@ -1,70 +1,35 @@
-#if !defined(BOOST_PP_IS_ITERATING)
-
 // Copyright David Abrahams 2002.
 // Distributed under the Boost Software License, Version 1.0. (See
 // accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 
-# ifndef CALL_DWA2002411_HPP
-#  define CALL_DWA2002411_HPP
+#ifndef CALL_DWA2002411_HPP
+# define CALL_DWA2002411_HPP
 
 # include <boost/python/detail/prefix.hpp>
 
-#  include <boost/type.hpp>
-
-#  include <boost/python/converter/arg_to_python.hpp>
-#  include <boost/python/converter/return_from_python.hpp>
-#  include <boost/python/detail/preprocessor.hpp>
-#  include <boost/python/detail/void_return.hpp>
-
-#  include <boost/preprocessor/comma_if.hpp>
-#  include <boost/preprocessor/iterate.hpp>
-#  include <boost/preprocessor/repeat.hpp>
-#  include <boost/preprocessor/debug/line.hpp>
-#  include <boost/preprocessor/repetition/enum_trailing_params.hpp>
-#  include <boost/preprocessor/repetition/enum_binary_params.hpp>
+# include <boost/python/converter/arg_to_python.hpp>
+# include <boost/python/converter/return_from_python.hpp>
 
 namespace boost { namespace python {
 
-# define BOOST_PYTHON_FAST_ARG_TO_PYTHON_GET(z, n, _) \
-    , converter::arg_to_python<A##n>(a##n).get()
+namespace detail {
+    template<typename T>
+    constexpr char O() { return 'O'; }
+}
 
-#  define BOOST_PP_ITERATION_PARAMS_1 (3, (0, BOOST_PYTHON_MAX_ARITY, <boost/python/call.hpp>))
-#  include BOOST_PP_ITERATE()
-
-#  undef BOOST_PYTHON_FAST_ARG_TO_PYTHON_GET
-
-}} // namespace boost::python
-
-# endif // CALL_DWA2002411_HPP
-
-// For gcc 4.4 compatability, we must include the
-// BOOST_PP_ITERATION_DEPTH test inside an #else clause.
-#else // BOOST_PP_IS_ITERATING
-#if BOOST_PP_ITERATION_DEPTH() == 1
-# if !(BOOST_WORKAROUND(__MWERKS__, > 0x3100)                      \
-        && BOOST_WORKAROUND(__MWERKS__, BOOST_TESTED_AT(0x3201)))
-#  line BOOST_PP_LINE(__LINE__, call.hpp)
-# endif
-
-# define N BOOST_PP_ITERATION()
-
-template <
-    class R
-    BOOST_PP_ENUM_TRAILING_PARAMS_Z(1, N, class A)
-    >
-typename detail::returnable<R>::type
-call(PyObject* callable
-    BOOST_PP_COMMA_IF(N) BOOST_PP_ENUM_BINARY_PARAMS_Z(1, N, A, const& a)
-    , boost::type<R>* = 0
-    )
+template<typename R, typename... Args>
+typename detail::returnable<R>::type 
+    call(PyObject* callable, Args const&... args)
 {
-    PyObject* const result = 
-        PyEval_CallFunction(
-            callable
-            , const_cast<char*>("(" BOOST_PP_REPEAT_1ST(N, BOOST_PYTHON_FIXED, "O") ")")
-            BOOST_PP_REPEAT_1ST(N, BOOST_PYTHON_FAST_ARG_TO_PYTHON_GET, nil)
-            );
+    // Format should be "(OOOO...)" with as many O's as there are arguments
+    constexpr char format[] = {'(', detail::O<Args>()..., ')', '\0'};
+    
+    PyObject* const result = PyEval_CallFunction(
+        callable, 
+        const_cast<char*>(format),
+        converter::arg_to_python<Args>(args).get()...
+    );
     
     // This conversion *must not* be done in the same expression as
     // the call, because, in the special case where the result is a
@@ -77,7 +42,6 @@ call(PyObject* callable
     return converter(result);
 }
 
-# undef N
+}} // namespace boost::python
 
-#endif // BOOST_PP_ITERATION_DEPTH()
-#endif
+#endif // CALL_DWA2002411_HPP
