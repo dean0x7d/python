f77fc91a12bf2d920b6bbc67568b34b4d0d2427b
Dean Moldovan <dean0x7d@gmail.com>
2014-08-13 13:31:27 +0200

    Variadic get_signature

--- include/boost/python/cpp14/type_traits.hpp
+++ include/boost/python/cpp14/type_traits.hpp
@@ -9,4 +9,7 @@ using remove_cv_t = typename std::remove_cv<T>::type;
 template<typename T>
 using add_cv_t = typename std::add_cv<T>::type;
 
+template<bool B, class T, class F>
+using conditional_t = typename std::conditional<B, T, F>::type;
+    
 }
---
+++ include/boost/python/detail/get_signature.hpp
@@ -0,0 +1,46 @@
+#ifndef BOOST_PYTHON_FN_CC
+#error Do not include this file
+#endif
+
+// as 'get_signature(RT(*)(Ts...), void*)' is the same
+// function as 'get_signature(RT(__cdecl *)(Ts...), void*)',
+// we don't define it twice
+#if !defined(BOOST_PYTHON_FN_CC_IS_CDECL)
+
+template <class RT, class... Ts>
+inline type_list<RT, Ts...> 
+get_signature(RT(BOOST_PYTHON_FN_CC *)(Ts...), void* = nullptr)
+{
+    return {};
+}
+
+#endif
+
+
+template <class RT, class ClassT, class... Ts, class Target = ClassT>
+inline type_list<RT, most_derived_t<Target, ClassT>&, Ts...>
+get_signature(RT(BOOST_PYTHON_FN_CC ClassT::*)(Ts...), Target* = nullptr)
+{
+    return {};
+}
+
+template <class RT, class ClassT, class... Ts, class Target = ClassT>
+inline type_list<RT, most_derived_t<Target, ClassT>&, Ts...>
+get_signature(RT(BOOST_PYTHON_FN_CC ClassT::*)(Ts...) const, Target* = nullptr)
+{
+    return {};
+}
+
+template <class RT, class ClassT, class... Ts, class Target = ClassT>
+inline type_list<RT, most_derived_t<Target, ClassT>&, Ts...>
+get_signature(RT(BOOST_PYTHON_FN_CC ClassT::*)(Ts...) volatile, Target* = nullptr)
+{
+    return {};
+}
+
+template <class RT, class ClassT, class... Ts, class Target = ClassT>
+inline type_list<RT, most_derived_t<Target, ClassT>&, Ts...>
+get_signature(RT(BOOST_PYTHON_FN_CC ClassT::*)(Ts...) const volatile, Target* = nullptr)
+{
+    return {};
+}
