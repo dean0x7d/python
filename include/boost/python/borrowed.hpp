00b6716ba1c46283e89b49d78504b63a0027533b
Dean Moldovan <dean0x7d@gmail.com>
2015-01-29 15:41:01 +0100

    Simplified is_borrowed_ptr and merged into borrowed.hpp header

--- include/boost/python/borrowed.hpp
+++ include/boost/python/borrowed.hpp
@@ -6,16 +6,37 @@
 # define BORROWED_DWA2002614_HPP
 
 # include <boost/python/detail/prefix.hpp>
-# include <boost/python/detail/borrowed_ptr.hpp>
+# include <boost/python/cpp14/type_traits.hpp>
+# include <boost/python/detail/is_xxx.hpp>
+# include <boost/python/tag.hpp>
 
-namespace boost { namespace python { 
+namespace boost { namespace python {
+
+namespace detail {
+    template<class T>
+    class borrowed {
+        using type = T;
+    };
+
+    template<class T>
+    using is_borrowed_ptr = std::integral_constant<bool,
+        std::is_pointer<T>::value &&
+            detail::is_<borrowed, cpp14::remove_cv_t<cpp14::remove_pointer_t<T>>>::value
+    >;
+}
 
 template <class T>
 inline python::detail::borrowed<T>* borrowed(T* p)
 {
     return (detail::borrowed<T>*)p;
 }
-    
+
+template <class T>
+inline T* get_managed_object(detail::borrowed<T> const volatile* p, tag_t)
+{
+    return (T*)p;
+}
+
 }} // namespace boost::python
 
 #endif // BORROWED_DWA2002614_HPP
