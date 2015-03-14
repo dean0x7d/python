a0f0a03323a0427f134f6fb89b428375da93fd9d
Dean Moldovan <dean0x7d@gmail.com>
2015-03-15 00:44:40 +0100

    Add type trait: is_base_template_of

--- include/boost/python/detail/is_xxx.hpp
+++ include/boost/python/detail/is_xxx.hpp
@@ -20,6 +20,23 @@ struct is_ : std::false_type {};
 template<template<class...> class Class, class... Args>
 struct is_<Class, Class<Args...>> : std::true_type {};
 
+//
+//  Test if a template is the base of T.
+//
+//  E.g.: "is_base_template_of<Class, T>::value" will return true
+//        if T is derived from Class<U> where U is anything.
+//
+
+template<template<class...> class Class, class T>
+struct is_base_template_of {
+    template<class... Args>
+    static std::true_type is_convertible(Class<Args...>*);
+    static std::false_type is_convertible(...);
+
+    using type = decltype(is_convertible(std::declval<T*>()));
+    static constexpr bool value = type::value;
+};
+
 }}} // namespace boost::python::detail
 
 #endif // IS_XXX_DWA2003224_HPP
